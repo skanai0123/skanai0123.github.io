148f12198ed466adc4db5c79eb9bc4fa3e2dac8e
skanai0123 <kanaishunriec@gmail.com>
2026-03-04 10:09:45 +0900

    Add shared Report Bug link to Back to Hub across playground pages

--- shared/back-to-hub.js
+++ shared/back-to-hub.js
@@ -9,10 +9,40 @@
   nav.setAttribute("aria-label", "Navigation");
 
   var link = document.createElement("a");
-  link.className = "shared-back-link";
+  link.className = "shared-nav-link shared-back-link";
   link.href = "https://skanai0123.github.io/";
   link.textContent = "Back to Hub";
 
+  var report = document.createElement("a");
+  report.className = "shared-nav-link shared-report-link";
+  report.target = "_blank";
+  report.rel = "noopener noreferrer";
+  report.textContent = "Report Bug";
+
+  var formBase = "https://docs.google.com/forms/d/e/1FAIpQLSeeOYJFGdl41iEUR_sJ9tm7XFoa8xzdoNlXP7ZflNwjPkf8pQ/viewform";
+  var entryPlaygroundName = "entry.1663149615";
+  var path = window.location.pathname.replace(/\/+$/, "");
+  var segs = path.split("/").filter(Boolean);
+  var pageName = "Hub";
+  if (segs.length > 0) {
+    var last = segs[segs.length - 1];
+    if (/\.html?$/i.test(last)) {
+      if (/^index(\..+)?\.html?$/i.test(last) || /^index\.html?$/i.test(last)) {
+        pageName = segs.length >= 2 ? segs[segs.length - 2] : "Hub";
+      } else {
+        pageName = last.replace(/\.html?$/i, "");
+      }
+    } else {
+      pageName = last;
+    }
+  }
+  var params = new URLSearchParams({
+    usp: "pp_url"
+  });
+  params.set(entryPlaygroundName, pageName);
+  report.href = formBase + "?" + params.toString();
+
   nav.appendChild(link);
+  nav.appendChild(report);
   document.body.insertBefore(nav, document.body.firstChild);
 })();
